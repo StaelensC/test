e9d973fd0091020c30363c19374f459a9976bbe2
Cedric Staelens <cedric.staelens@student.hogent.be>
2018-10-10 17:10:12 +0200

    test

--- H2FibonacciOef1/Program.cs
+++ H2FibonacciOef1/Program.cs
@@ -6,7 +6,29 @@ namespace H2FibonacciOef1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result;
+            Console.WriteLine("Welk getal uit de rij van Fibonacci wilt u kennen?");
+            int index = int.Parse(Console.ReadLine());
+            result = GetFibonacci(index);
+            Console.WriteLine($"Het getal dat u zocht is {result}");
+            
+
+        }
+
+        private static int GetFibonacci(int index) {
+            if (index == 1 || index == 2)
+                return --index;
+            int Fibonacci1 = 0;
+            int Fibonacci2 = 1;
+            int result = 0;
+            while (index > 2) {
+                result = Fibonacci1 + Fibonacci2;
+                Fibonacci1 = Fibonacci2;
+                Fibonacci2 = result;
+                
+                index--;
+            }
+            return result;
         }
     }
 }
